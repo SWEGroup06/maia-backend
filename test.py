d13009ce59434a2165b6ff928b623de4f1e00020
Samuel Zubaid <samuelzubaid@outlook.com>
2020-10-13 16:20:06 +0100

    CICD: Simplified config.yml and add unit test file

---
+++ test.py
@@ -0,0 +1,11 @@
+import hello_world
+import unittest
+
+class Tester(unittest.TestCase):
+
+    def test(self):
+        result = "hello_world"
+        self.assertEqual(hello_world.hello.hi(), result)
+
+if __name__ == '__main__':
+    unittest.main()
