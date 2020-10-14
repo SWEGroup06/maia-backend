588bbb06199af357af06179f73efb117092e6675
Amelia Khavari <ameliakh2@gmail.com>
2020-10-14 17:51:07 +0100

    scheduler@BE: further updated code style inline with PEP8

--- test/test_scheduler.py
+++ test/test_scheduler.py
@@ -7,20 +7,21 @@ from datetime import datetime, timedelta
 from src import scheduler as sch
 
 FREE_DATETIME1 = [(datetime(2020, 10, 14, 9 + 3 * x), datetime(2020, 10, 14, 10 + 3 * x))
-                   for x in range(5)]
+                  for x in range(5)]
 FREE_DATETIME2 = [(datetime(2020, 10, 14, 9 + 4 * x), datetime(2020, 10, 14, 10 + 4 * x))
-                   for x in range(4)]
+                  for x in range(4)]
 
 FREE_DATETIME3 = [(datetime(2020, 10, 14, 10), datetime(2020, 10, 14, 12)),
-                   (datetime(2020, 10, 14, 15), datetime(2020, 10, 14, 20)),
-                   (datetime(2020, 10, 14, 21), datetime(2020, 10, 14, 22, 30))]
+                  (datetime(2020, 10, 14, 15), datetime(2020, 10, 14, 20)),
+                  (datetime(2020, 10, 14, 21), datetime(2020, 10, 14, 22, 30))]
 FREE_DATETIME4 = [(datetime(2020, 10, 14, 9), datetime(2020, 10, 14, 12)),
-                   (datetime(2020, 10, 14, 12, 30), datetime(2020, 10, 14, 13)),
-                   (datetime(2020, 10, 14, 20), datetime(2020, 10, 14, 21))]
+                  (datetime(2020, 10, 14, 12, 30), datetime(2020, 10, 14, 13)),
+                  (datetime(2020, 10, 14, 20), datetime(2020, 10, 14, 21))]
 
 ALL_FREE_DATETIMES = [FREE_DATETIME1, FREE_DATETIME2]
 
 DURATION = timedelta(minutes=30)
+
 
 class TestScheduler(TestCase):
     def test_intersects(self):
