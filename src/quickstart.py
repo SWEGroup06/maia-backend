cc9dd7680ff9fa1d5deef1d8c4d2a9b0926d1a52
Ihowa <ihowaonaro@gmail.com>
2020-10-15 21:15:58 +0100

    ApiIntegration: created functions to provide busy data of users.

--- src/quickstart.py
+++ src/quickstart.py
@@ -12,8 +12,8 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def main():
     """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+        Prints the start and name of the next 10 events on the user's calendar.
+        """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -26,29 +26,58 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
-
+    
     service = build('calendar', 'v3', credentials=creds)
-
+    
     # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
+    events = getEvents(service)
     if not events:
         print('No upcoming events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
 
+    
+    timeMin = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    timeMax = datetime.datetime(2020, 10, 18, 3).isoformat() + 'Z'
+    print(timeMin)
+    print(timeMax)
+    print('######################')
+    getFreeBusy(service, timeMin, timeMax)
+
+
+
+def getEvents(service):
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    return events
+
+def getFreeBusy(service, timeMin, timeMax):
+    #body might have to be created by bot
+    body = {
+        "timeMin": timeMin,
+            "timeMax": timeMax,
+            "items": [
+                      {
+                      "id": 'primary'
+                
+                      }
+                      ]
+    }
+    response = service.freebusy().query(body=body).execute()
+    busy_data = response['calendars']
+    return busy_data
+
 
 if __name__ == '__main__':
     main()
+
