15da532e2094a26d553c6eb8fe057c671b888c9d
a-abidi <aliabidi@icloud.com>
2020-10-15 11:20:49 +0100

    CICD@BE: Change test.py to be more in-line with pep8.

--- test/test.py
+++ test/test.py
@@ -1,10 +1,19 @@
 import unittest
 from src import hello_world
 
+
 class MyTestCase(unittest.TestCase):
+    """
+    Test cases for Maia backend.
+    TODO: Improve this docstring
+    """
     def test_something(self):
+        """
+
+        :return:
+        """
         result = "hello_world"
-        self.assertEqual(hello_world.hello().hi(), result)
+        self.assertEqual(hello_world.say_hello_world(), result)
 
 
 if __name__ == '__main__':
