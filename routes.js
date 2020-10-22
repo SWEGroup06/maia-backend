b1b325ab0bdaf57adad8aac0a3671acd5a172e07
Ali <aliabidi@icloud.com>
2020-10-22 14:42:51 +0100

    eslint@BE: Fix code style of routes and srever.

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", function(req, res) {
-  res.send( "Hello World");
+router.get('/', function(req, res) {
+  res.send( 'Hello World');
 });
 
 module.exports = router;
