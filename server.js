b2f8de82422053162f223629fccdeeffbfcfd425
Samuel Zubaid <samuelzubaid@outlook.com>
2020-10-22 14:22:41 +0100

    node@BE: rename routes.js and add mongoose dependency

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const routes = require('./routes');
+const routesRouter = require('./routes/route');
+const mongoose = require('mongoose');
 
 const app = express()
 
 const port = process.env.PORT || "3000";
 
-app.use('/', routes)
+app.use('/', routesRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
