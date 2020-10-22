8859b7c888ef68ac32eedccf62981e64a3bbb7a8
Ali <aliabidi@icloud.com>
2020-10-22 18:21:12 +0100

    server@BE: Change server message.

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.send( "Hello World");
+  res.send( "Maia Server is running.");
 });
 
 module.exports = router;
