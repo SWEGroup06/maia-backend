68d36950a8200c62a34e736e0f50a36a7fe4cf86
KPal81 <kpal81xd@gmail.com>
2020-10-22 17:03:06 +0100

    Added extra commands

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const routes = require('./api/routes');
 
 const app = express();
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || 3000;
 
 app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`API Server hosted on: http://localhost:${port}`);
 });
