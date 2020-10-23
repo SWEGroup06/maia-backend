95cc0972157609a6bebb0ac6534e44ef4b6b924f
Ali <aliabidi@icloud.com>
2020-10-23 14:31:37 +0100

    database@BE: Add teamID to user model.

--- models/user.model.js
+++ models/user.model.js
@@ -7,6 +7,10 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  teamID: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
