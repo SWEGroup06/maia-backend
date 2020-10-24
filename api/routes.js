3efbb6b850fab1f2e51e95059c6637e9278787dc
KPal81 <kpal81xd@gmail.com>
2020-10-24 18:31:16 +0100

    Reverted changes (issue in createNewUser)

--- api/routes.js
+++ api/routes.js
@@ -49,9 +49,8 @@ router.get('/oauth2callback', async function(req, res) {
   try {
     // const state = JSON.parse(decodeURIComponent(req.query.state));
 
-    await AUTH.getTokens(req.query.code);
-    res.redirect('success');
-    // await DATABASE.instance.createNewUser(state.userID, state.teamID, tokens);
+    const tokens = await AUTH.getTokens(req.query.code);
+    await DATABASE.instance.createNewUser(state.userID, state.teamID, tokens);
 
     // Redirect to success page
     res.redirect('success');
