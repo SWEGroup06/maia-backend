c207bb3942dce3e57103bb62401417a29c1ac1df
Ali <aliabidi@icloud.com>
2020-10-26 16:16:30 +0000

    database: Move all functions from users.js to database.js.

--- api/database.js
+++ api/database.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
-
 // Load User model
 const User = require('../models/user.model');
+
+// Load environment variables
+require('dotenv').config();
 
 /*
  * TODO List:
@@ -11,101 +12,119 @@ const User = require('../models/user.model');
  *  - Return console logs for each function
  */
 
+
 module.exports = {
-  connect: function(uri) {
-    // Connect to MongoDB using the provided URI
-    mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
+  /**
+   * Creates a new user in the database, uniquely identified by User ID, Team ID and token
+   * @param {string} email
+   * @param {string} token
+   * @return {boolean} success
+   */
+  createNewUser: function(email, token) {
+    /**
+     * Non-existent constraints for time are represented as empty string
+     * @return {Array} Non-existent constraints for seven days
+     **/
+    function initialiseConstraints() {
+      return [{startTime: '', endTime: ''},
+        {startTime: '', endTime: ''},
+        {startTime: '', endTime: ''},
+        {startTime: '', endTime: ''},
+        {startTime: '', endTime: ''},
+        {startTime: '', endTime: ''},
+        {startTime: '', endTime: ''}];
+    }
+
+    const user = new User({
+      email: email,
+      token: token,
+      constraints: initialiseConstraints(),
     });
 
-    return new Promise(function(resolve) {
-      mongoose.connection.once('open', function() {
-        console.log('Database connected');
-        module.exports.instance = {
-          /**
-           * Creates a new user in the database, uniquely identified by User ID, Team ID and token
-           * @param {string} userID
-           * @param {string} teamID
-           * @param {string} token
-           * @return {boolean} success
-           */
-          createNewUser: function(userID, teamID, token) {
-            const user = new User({
-              id: {userID, teamID},
-              token,
-            });
+    let success = false;
 
-            return new Promise(function(resolve, reject) {
-              user.save(function(err) {
-                if (err) {
-                  console.log(`[Error creating new user]\n${err}`);
-                  reject(err);
-                  return;
-                }
-
-                console.log(`[Created new user] userID: ${userID}, teamID: ${teamID}`);
-                resolve();
-              });
-            });
-          },
-
-          /**
-           * Retrieves the token of a user given the User ID and Team ID which uniquely
-           * identifies a user.
-           * @param {String} userID
-           * @param {String} teamID
-           * @return {String} If token exists return it in String format, otherwise returns null.
-           */
-          getToken: function(userID, teamID) {
-            return new Promise(function(resolve, reject) {
-              User.findOne({id: {userID, teamID}}).then(function(user) {
-                resolve(user && user.token ? user.token : null);
-              }).catch(reject);
-            });
-          },
-
-          /**
-           * Updates a user's token value with a new token, given the User ID and Team ID
-           * which uniquely identifies a user.
-           * @param {String} userID
-           * @param {String} teamID
-           * @param {String} token
-           * @return {boolean} Returns true if successful, false if token already exists in database.
-           */
-          updateToken: async function(userID, teamID, token) {
-            if (await this.tokenExists(token)) {
-              console.log('[Error updating token] Token already exists in database');
-              return false;
-            } else {
-              User.findOneAndUpdate({id: {userID, teamID}}, {id: {userID, teamID}, token});
-              console.log('[Token updated successfully]');
-              return true;
-            }
-          },
-
-          /**
-           * Checks if the user, identified by a User ID and Team ID, exists in the database.
-           * @param {String} userID
-           * @param {String} teamID
-           * @return {boolean} Returns true if there exists a unique userID and teamID pair.
-           */
-          userExists: async function(userID, teamID) {
-            return await User.exists({id: {userID, teamID}});
-          },
-
-          /**
-           * Checks if the given token exists for any user in the database
-           * @param {String} token
-           * @return {boolean} Returns true is token exists in database, false otherwise.
-           */
-          tokenExists: async function(token) {
-            return await User.exists({token});
-          },
-        };
-        resolve();
-      });
+    user.save((error) => {
+      if (error) {
+        console.log('[Error creating new user] \n' + error);
+        success = false;
+      } else {
+        console.log('[Created new user] Email: ' + email);
+        success = true;
+      }
     });
+
+    return success;
+  },
+
+  /**
+   * Retrieves token of a user given the email address which uniquely identifies a user.
+   * @param {String} email
+   * @return {String} If token exists return it in String format, otherwise returns null.
+   */
+  getToken: async function(email) {
+    let token = null;
+    User.findOne({email: email}).then((user) => {
+      if (user == null) {
+        token = null;
+      } else {
+        token = user.token;
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+    return token;
+  },
+
+  /**
+   * Updates a user's token value with a new token, given the User ID and Team ID
+   * which uniquely identifies a user.
+   * @param {String} email
+   * @param {String} newToken
+   * @return {boolean} Returns true if successful, false if token already exists in database.
+   */
+  updateToken: async function(email, newToken) {
+    if (await this.tokenExists(newToken)) {
+      console.log('[Error updating token] Token already exists in database');
+      return false;
+    } else {
+      User.findOneAndUpdate({email: email}, {email: email, token: newToken});
+      console.log('[Token updated successfully]');
+      return true;
+    }
+  },
+
+  /**
+   * Checks if the user, identified by a User ID and Team ID, exists in the database.
+   * @param {String} email
+   * @return {boolean} Returns true if there exists a user registered with this email.
+   */
+  userExists: async function(email) {
+    return await User.exists({email: email});
+  },
+
+  /**
+   * Checks if the given token exists for any user in the database
+   * @param {String} token
+   * @return {boolean} Returns true is token exists in database, false otherwise.
+   */
+  tokenExists: async function(token) {
+    return await User.exists({token: token});
+  },
+
+  /**
+   * TODO: Comment
+   * @param {String} email
+   * @param {String} startTime represented in ISO format
+   * @param {String} endTime represented in ISO format
+   * @param {Number} dayOfWeek - TODO: Amelia + Hasan create constants where Monday is 0
+   */
+  setConstraint: async function(email, startTime, endTime, dayOfWeek) {
+    await User.findOneAndUpdate(
+        {'email': email},
+        {'$set': {
+          [`constraints.${dayOfWeek}.startTime`]: Time.getTimeFromISO(startTime),
+          [`constraints.${dayOfWeek}.endTime`]: Time.getTimeFromISO(endTime),
+        }},
+    );
   },
 };
