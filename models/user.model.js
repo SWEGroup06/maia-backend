fdcef794d07149b9e45ebbac7d89e8a8565eb2b6
Ali <aliabidi@icloud.com>
2020-10-29 19:55:37 +0000

    database: Add ID to user schema.

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  id: {
+    type: String,
+    unique: true,
+  },
   email: {
     type: String,
     unique: true,
