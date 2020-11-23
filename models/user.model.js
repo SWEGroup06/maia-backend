30482501a6832b42f5be868ac620e6719f9ba6f9
Samuel Zubaid <samuelzubaid@outlook.com>
2020-11-23 13:05:03 +0000

    add frequencies table to model

--- models/user.model.js
+++ models/user.model.js
@@ -18,7 +18,8 @@ const UserSchema = new Schema({
     required: true,
   },
   google: GoogleAccountSchema,
-  constraints: [[{startTime: String, endTime: String}]],
+  constraints: [{startTime: String, endTime: String}],
+  frequencies: [[Number]],
 });
 
 const User = mongoose.model('User', UserSchema);
