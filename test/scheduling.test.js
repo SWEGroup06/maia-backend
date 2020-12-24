8f0c56b813d3b473e995dd2c3565154dcec36243
Ali <aliabidi@icloud.com>
2020-12-24 11:13:24 +0000

    jest: Create scheduling test file.

---
+++ test/scheduling.test.js
@@ -0,0 +1,10 @@
+/**
+ * @fileOverview This test file holds various test cases regarding the scheduling/creation of
+ * meetings/events by the user.
+ * @package
+ */
+
+const DIALOGFLOW = require('../lib/dialogflow.js');
+const GOOGLE = require('../lib/google.js');
+const DATABASE = require('../lib/database.js');
+const MEETINGS = require('../lib/meetings.js');
