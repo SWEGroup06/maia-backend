021e7c07f2a7b59cbf7a1110d54f056c52c610e2
KPal81 <kpal81xd@gmail.com>
2020-12-24 20:55:09 +0000

    Linked webhook with imap email retrieval

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// Determines which command the user wants
-router.post('/email', async function(req, res) {
-  console.log(req, res);
-  res.sendStatus(200);
-});
+const IMAP = require('../lib/imap.js');
+IMAP.init().then(() => {
+  // Gmail Pub/Sub callback
+  router.post('/email', async function(req, res) {
+    IMAP.getLatestEmail().then(console.log).catch(console.error);
+    res.sendStatus(200);
+  });
+}).catch(console.error);
 
 module.exports = router;
--- webhook.js
+++ webhook.js
@@ -8,7 +8,7 @@ const uuid = require('uuid');
 const fs = require('fs');
 const path = require('path');
 
-const token = "<TOKEN>";
+const token = {"access_token":"ya29.a0AfH6SMBZzeu3HqXtOqlBWCYscDf_9UrTK8n06JKUZsiVcQEM3qI6T1V-cS_hCl1OrOggd-GLg7eZWt7Z7BOtr7o0t5xmsrEceSD7Rd6IWWMhOHZ7os9kuafM3qMP1dwwiZVEv-pncIuADVZ7C5GQAP0qAL14KYOxfBHM5WgR1Zg","refresh_token":"1//07HLlGExgZhLfCgYIARAAGAcSNwF-L9IrTh_q09FeMnTrFU9vRikUsjBlFqvLf0dbFmZHeBdEg9-V2FAgy-3_Lm-CrBR3EfGtJiE","scope":"https://www.googleapis.com/auth/gmail.send openid https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.settings.basic","token_type":"Bearer","id_token":"eyJhbGciOiJSUzI1NiIsImtpZCI6IjZhZGMxMDFjYzc0OThjMDljMDEwZGMzZDUxNzZmYTk3Yzk2MjdlY2IiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3NTM4OTUxMzcwODYtNWFpbG9iNWQ2N2JrYmNnOTRka2RjMW5ydDFtcW4yOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3NTM4OTUxMzcwODYtNWFpbG9iNWQ2N2JrYmNnOTRka2RjMW5ydDFtcW4yOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDc3ODExMjA0MDkyODUxNzMwNDAiLCJlbWFpbCI6Im1haWFjYWxlbmRhcjEyM0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6Ilh6el9fcVZwMXJNUGxyM0RUckhmS3ciLCJuYW1lIjoiTWFpYSBBSSBDYWxlbmRhciBBc3Npc3RhbnQiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R2hWcDNQbFNwbG01aWNsTC1EWDlHSzlPZ0RSY3VvZWpOcC1fUHhyPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6Ik1haWEiLCJmYW1pbHlfbmFtZSI6IkFJIENhbGVuZGFyIEFzc2lzdGFudCIsImxvY2FsZSI6ImVuLUdCIiwiaWF0IjoxNjA4ODM1MDQ4LCJleHAiOjE2MDg4Mzg2NDh9.UrxRRgTSGJgVV_PsSMs0Z7lHEpIVTLocSMS_BHyQudHxW3bP-2ZVeUCyGgPP33Lwrk-VGlCYlA9tkPuyL67tVQ3_8lEQFwbE8ueFbp6oAlLHD29cIJCFFBUVRspSutNRGVqk4B0wM-s5ptuq-saS96Nz6zDWIAKTx0Ol2-aVYhU2p1w4yfHpmV8BIiM4MPtNoNnnGvixfOStcvkFX8wOy8wOenaJK-Iu3VHQp-WmhoVGvPKqspqVJe8RCh6Oh7c3kWiHcSYZPffUzoX2OgBWBmCBAxJBdW4p8AjMIuzSEStnMu1dLTokem3w3uVRmN8XWwR3hLc5LqylkGd0gTHVeA","expiry_date":1608838647546};
 
 (async () => {
   try {
@@ -19,11 +19,11 @@ const token = "<TOKEN>";
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        topicName: 'projects/maiacalendarproj-1602934092148/topics/Meetings',
+        topicName: 'projects/avid-renderer-293013/topics/Meetings',
         resourceId: ["INBOX"],
       }),
     });
-    console.log(res);
+    console.log(await res.text());
     process.exit(0);
   } catch (err) {
     console.error('ERROR: ', err.stack);
