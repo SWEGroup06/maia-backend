8a043b753cff93d3152c05c949935413a41b6df3
Ali <aliabidi@icloud.com>
2020-12-25 09:46:05 +0000

    mocha: Testing async not working need help.

--- test/meeting.test.js
+++ test/meeting.test.js
@@ -6,20 +6,35 @@ const GOOGLE = require('../lib/google.js');
 const DATABASE = require('../lib/database.js');
 const MEETINGS = require('../lib/meetings.js');
 
+const mocha = require('mocha');
+const describe = mocha.describe;
+const it = mocha.it;
+
 describe('Scheduling in a given range', () => {
-  it('should book correctly', () => {
+  it('should book correctly', async () => {
     const tomorrow = DateTime.local().plus({days: 1});
     const tomorrowTwelveHours = DateTime.local(tomorrow.year, tomorrow.month, tomorrow.day, 12);
     const tomorrowEighteenHours = DateTime.local(tomorrow.year, tomorrow.month, tomorrow.day, 18);
 
+    console.log('HERE');
+
     const testEmail = 's.amnabidi@gmail.com'; // TODO: Change to a test email account
     const testToken = DATABASE.getToken(testEmail);
 
-    const meetingSlot = MEETINGS.schedule(undefined, [testEmail], tomorrowTwelveHours.toISO(),
+    console.log('HEREE');
+
+    const meetingSlot = await MEETINGS.schedule(undefined, [testEmail], tomorrowTwelveHours.toISO(),
         tomorrowEighteenHours.toISO(), true);
+    const events = await GOOGLE.getEvents(testToken, meetingSlot.start);
+
+    console.log('slackEmails: ' + [].push(testEmail));
+    console.log('tomorrowTwelveHours: ' + tomorrowTwelveHours.toISO());
+    console.log('tomorrowEighteenHours: ' + tomorrowEighteenHours.toISO());
+
+    console.log('HEREEE');
 
     console.log('* meetingSlot ' + meetingSlot);
-    console.log('* GOOGLE.getEvents(testToken, meetingSlot.start)[0]: ' + GOOGLE.getEvents(testToken, meetingSlot.start)[0]);
+    console.log('* events[0]: ' + events[0]);
 
     assert.strictEqual(true, true);
   });
