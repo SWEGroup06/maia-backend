36f304f5e745ab283d15350e7415db683c8dd8e5
Ali <aliabidi@icloud.com>
2020-12-26 16:58:18 +0000

    test-suite: Update test to make more sense.

--- test/meeting.test.js
+++ test/meeting.test.js
@@ -11,14 +11,14 @@ const MEETINGS = require('../lib/meetings.js');
 const mocha = require('mocha');
 const {describe, it, before, after} = mocha;
 
-describe('Scheduling in a given range', function() {
+describe('scheduling within a given range', function() {
   before(async () => {
     await DATABASE.getDatabaseConnection();
     const token = JSON.parse(await DATABASE.getToken('s.amnabidi@gmail.com'));
     await GOOGLE.clearCalendar(token);
   });
 
-  it('should book correctly', async () => {
+  it('should book a single one-hour event between 12pm and 6pm tomorrow', async () => {
     const tomorrow = DateTime.local().plus({days: 1});
     const tomorrowTwelveHours = DateTime.local(tomorrow.year, tomorrow.month, tomorrow.day, 12);
     const tomorrowEighteenHours = DateTime.local(tomorrow.year, tomorrow.month, tomorrow.day, 18);
@@ -27,10 +27,11 @@ describe('Scheduling in a given range', function() {
     const testEmails = ['s.amnabidi@gmail.com']; // TODO: Change to a test email account
     const testToken = JSON.parse(await DATABASE.getToken(testEmails[0]));
 
-    const meetingSlot = await MEETINGS.schedule(undefined, testEmails, tomorrowTwelveHours.toISO(),
+    // Book a flexible, untitled, one-hour meeting between 12pm and 6pm tomorrow
+    await MEETINGS.schedule(undefined, testEmails, tomorrowTwelveHours.toISO(),
         tomorrowEighteenHours.toISO(), true, ONE_HOUR);
 
-    const events = await GOOGLE.getEvents(testToken, meetingSlot.start);
+    const events = await GOOGLE.getEvents(testToken, tomorrowTwelveHours.toISO(), tomorrowEighteenHours.toISO());
 
     assert.strictEqual(events.length, 1);
   });
