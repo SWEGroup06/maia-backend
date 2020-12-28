619ba243ca590961973e66b2ba860ae326032ecc
KPal81 <kpal81xd@gmail.com>
2020-12-28 18:59:55 +0000

    Updated login process

--- routes/auth.js
+++ routes/auth.js
@@ -9,24 +9,31 @@ router.use('/success', express.static('public'));
 
 // Login
 router.get('/login', async function(req, res) {
-  if (!req.query.googleEmail) {
+  if (!req.query.googleEmail || (!!req.query.slackId + !!req.query.slackId) < 2) {
     res.json({error: 'No email provided'});
     return;
   }
 
   try {
-    const data = {
-      googleEmail: JSON.parse(decodeURIComponent(req.query.googleEmail)),
-    };
-    if ((!!req.query.slackId + !!req.query.slackId) == 2) {
+    const data = {};
+    if (req.query.googleEmail) {
       data.slackId = JSON.parse(decodeURIComponent(req.query.slackId));
       data.slackEmail = JSON.parse(decodeURIComponent(req.query.slackEmail));
-    }
 
-    // Check if a user with the provided details existing in the database
-    if (await DATABASE.userExists(data.googleEmail)) {
-      res.json({exists: true, googleEmail: data.email});
-      return;
+      // Check if a user with the provided details existing in the database
+      if (await DATABASE.userExists(data.slackEmail)) {
+        const email = DATABASE.getGoogleEmailFromSlackEmail(data.slackEmail);
+        res.json({exists: true, email});
+        return;
+      }
+    } else {
+      data.googleEmail = JSON.parse(decodeURIComponent(req.query.googleEmail));
+
+      // Check if a user with the provided details existing in the database
+      if (await DATABASE.userExists(data.googleEmail)) {
+        res.json({exists: true, email: data.email});
+        return;
+      }
     }
 
     // If no details were found send URL
@@ -51,11 +58,11 @@ router.get('/callback', async function(req, res) {
   try {
     const state = JSON.parse(decodeURIComponent(req.query.state));
 
-    const tokens = await GOOGLE.getTokenFromGoogleEmails(req.query.code);
+    const tokens = await GOOGLE.getToken(req.query.code);
     const googleEmail = await GOOGLE.getEmail(tokens);
     await DATABASE.createNewUser(googleEmail, JSON.stringify(tokens), state.slackEmail, state.slackId);
 
-    setTimeout(() => MEETINGS.generatePreferences(tokens, state.slackEmail), 0);
+    setTimeout(() => MEETINGS.generatePreferences(googleEmail, tokens), 0);
 
     // Redirect to success page
     res.redirect('success/login.html');
