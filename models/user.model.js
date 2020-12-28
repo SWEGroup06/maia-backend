19898997c1616b35e38bff28aed55261c87c9f01
Ali <aliabidi@icloud.com>
2020-12-28 16:25:34 +0000

    database: Seperate out schema for Slack and Google.

--- models/user.model.js
+++ models/user.model.js
@@ -3,22 +3,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GoogleAccountSchema = new Schema({
-  email: String, // Email associated with Google Calendar account
-  token: String, // Authorisation token per user for Google Calendar
+  email: {type: String, unique: true, required: true}, // Email associated with Google Calendar account
+  token: {type: String, required: true}, // Authorisation token per user for Google Calendar
 });
 
 const SlackAccountSchema = new Schema({
-  id: String, // Email associated with Google Calendar account
+  id: {type: String}, // ID associated with Slack account
+  email: {type: String, unique: true}, // Email associated with Slack account
 });
 
 const UserSchema = new Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  slack: SlackAccountSchema,
   google: GoogleAccountSchema,
+  slack: SlackAccountSchema,
   constraints: [[{startTime: String, endTime: String}]],
   frequencies: [{histFreq: [[Number]], timestamp: String}],
 });
