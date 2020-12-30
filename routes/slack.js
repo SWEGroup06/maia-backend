403958c6600cdb4760b2b7cdd1c285ec5d528bed
TaariqP <tp1717@ic.ac.uk>
2020-12-30 22:25:51 +0000

    Fix bug to do with meeting titles during edit and remove debug statements.

--- routes/slack.js
+++ routes/slack.js
@@ -36,8 +36,7 @@ const postMessage = async function(channelId, text) {
 
 /* Open a Modal View on the Slack App via Slack API */
 const openView = async function(view) {
-  console.log('DEBUG: OPENVIEW REACHED');
-  const res = await fetch('https://slack.com/api/views.open', {
+  await fetch('https://slack.com/api/views.open', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json; charset=utf-8',
@@ -45,8 +44,6 @@ const openView = async function(view) {
     },
     body: JSON.stringify(view),
   });
-  console.log(res);
-  console.log('DEBUG: posted view');
 };
 
 
@@ -139,7 +136,6 @@ const actionHandlers = {
         const text = 'Your meeting booking has been cancelled';
         await submitResponse(payload, {text});
       } else if (action.action_id == 'edit') {
-        console.log('DEBUG: CONFIRM REACHED');
         EDIT_MEETING_VIEW.trigger_id = payload.trigger_id;
         openView(EDIT_MEETING_VIEW);
       }
